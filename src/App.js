e57b10a8e32ab1204554b1cf369fe9e46c627eec
Weezy-88 <145158306+Weezy-88@users.noreply.github.com>
2024-06-21 21:02:18 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import React from "react";
 import "./App.css";
-
 export default function App() {
   return (
     <div className="App">
-      <p>
-        This website was coded by Louise Duff and is open-sourced on{" "}
-        <a
-          href="https://github.com/Weezy-88/weather-app-react2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>{" "}
-      </p>
+      <div className="container">
+        <p>
+          This website was coded by Louise Duff and is open-sourced on{" "}
+          <a
+            href="https://github.com/Weezy-88/weather-app-react2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+        </p>
+      </div>
     </div>
   );
 }
